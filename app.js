00e9b60d587221f72b9a2d37fe6f6336f57bd1ff
Igor <pinheiro.ir08@gmail.com>
2026-09-25 10:44:33 -0300

    rollback1

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-const { const1 } = require('./interface.js');
-
-//console.log(const1);
-
-
-//import {GoogleGenAI} from '@google/genai';
-
 const express = require('express');
 const app = express();
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send(const1);
+  res.send('<p>home page aqui</p>');
 });
 
 app.get('/link1', (req, res) => {
@@ -21,16 +14,3 @@ app.get('/link1', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('<p>nops... pagina errada</p>');
 });
-
-//const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
-/*
-async function main(pergunta) {
-  const response = await ai.models.generateContent({
-    model: 'gemini-flash-latest',
-    contents: pergunta,
-  });
-  //console.log(response.text);
-}
-
-main();
-*/
--- interface.js
+++
@@ -1,3 +0,0 @@
-const1 = '<div style="position:absolute;display:flex;flex-direction:column;"><textarea id="mensagem" name="mensagem" rows="5" cols="40" placeholder="Digite seu texto aqui..."></textarea><button onClick={handleSendPrompt} style="padding:10px 20px; cursor: pointer; margin-top: 10px;">Enviar Prompt</button><div id="resposta" style="margin-top: 20px;"></div></div>';
-
-module.exports = {const1};
